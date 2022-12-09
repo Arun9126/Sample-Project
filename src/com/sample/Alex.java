89176153cf94629ebe11e29a143447974aeb27e5
Arun9126 <nuras1991@gmail.com>
2022-12-09 10:37:18 +0530

    messager

---
+++ src/com/sample/Alex.java
@@ -0,0 +1,25 @@
+package com.sample;
+
+import java.util.Scanner;
+
+public class Alex {
+	public static void main(String[] args) {
+	
+
+	
+	int y,z;
+
+	Scanner arun=new Scanner (System.in);
+	System.out.println("Value of a is:");
+
+	y=arun.nextInt();
+	System.out.println("Value of b is:");
+
+	z=arun.nextInt();
+	int s=y*z;
+System.out.println("Now the total value is s:" + s);
+
+
+}
+
+}
--- src/com/sample/Sample.java
+++ src/com/sample/Sample.java
@@ -5,10 +5,55 @@ import java.util.Scanner;
 public class Sample {
 	
 	public static void main(String[] args) {
-		Scanner scanner= new Scanner(System.in);
-		System.out.println("What is your name");
-		String name=scanner.nextLine();
-		System.out.println(name);
+//		Scanner scanner= new Scanner(System.in);
+//		System.out.println("What is your name");
+//		String name=scanner.nextLine();
+//		System.out.println(name);
+		
+		int a=2,b=3;
+		int c=a+b;
+		System.out.println(c);
+		int d=a++;
+		System.out.println(d);
+		System.out.println(a>b);
+		String f= "alex";
+		char g='s';
+		
+		System.out.println(f+g);
+		
+				int j=20,k=23;
+		int l= ++j - --k;
+		System.out.println(l);
+		Scanner scanner= new Scanner (System.in);
+		String morning=scanner.nextLine();
+		System.out.println(morning);
+		System.out.println(j);
+		Scanner scanner1= new Scanner (System.in);
+		System.out.println("Enter a number: ");
+		int num = scanner1.nextInt();
+		if(num>2) {
+			System.out.println(num + "is big number");
+			
+		}
+		else {
+			System.out.println(num + "is small number");
+			Scanner scanner3= new Scanner (System.in);
+			String stella= scanner3.nextLine();
+			System.out.println(stella);
+			
+			
+			
+
+	
+		}
+		
+
+		
+		
+		
+		
+		
+		
 		
 		
 		
